c52143c5970f037a212407818e965417ffaed83f
jfhaugh <jfhaugh@43b03309-50c7-4bd2-8aa6-20f049331b16>
2012-06-23 22:48:13 +0000

    Simple code cleanup on com.ghgande.j2mod.modbus.cmd
    git-svn-id: svn://svn.code.sf.net/p/j2mod/code/trunk@15 43b03309-50c7-4bd2-8aa6-20f049331b16

--- src/com/ghgande/j2mod/modbus/cmd/SerialDITest.java
+++ src/com/ghgande/j2mod/modbus/cmd/SerialDITest.java
@@ -43,111 +43,109 @@ import com.ghgande.j2mod.modbus.util.BitVector;
 import com.ghgande.j2mod.modbus.util.SerialParameters;
 
 /**
- * Class that implements a simple commandline
- * tool for reading an analog input.
- *
+ * Class that implements a simple command line tool for reading an analog
+ * input.
+ * 
  * @author Dieter Wimberger
  * @version 1.2rc1 (09/11/2004)
  */
 public class SerialDITest {
+	public static void main(String[] args) {
 
-  public static void main(String[] args) {
+		SerialConnection con = null;
+		ModbusSerialTransaction trans = null;
+		ReadInputDiscretesRequest req = null;
+		ReadInputDiscretesResponse res = null;
 
-    SerialConnection con = null;
-    ModbusSerialTransaction trans = null;
-    ReadInputDiscretesRequest req = null;
-    ReadInputDiscretesResponse res = null;
+		String portname = null;
+		int unitid = 0;
+		int ref = 0;
+		int count = 0;
+		int repeat = 1;
 
-    String portname = null;
-    int unitid = 0;
-    int ref = 0;
-    int count = 0;
-    int repeat = 1;
+		try {
 
-    try {
+			// 1. Setup the parameters
+			if (args.length < 4) {
+				printUsage();
+				System.exit(1);
+			} else {
+				try {
+					portname = args[0];
+					unitid = Integer.parseInt(args[1]);
+					ref = Integer.parseInt(args[2]);
+					count = Integer.parseInt(args[3]);
+					if (args.length == 5) {
+						repeat = Integer.parseInt(args[4]);
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					printUsage();
+					System.exit(1);
+				}
+			}
 
-      //1. Setup the parameters
-      if (args.length < 4) {
-        printUsage();
-        System.exit(1);
-      } else {
-        try {
-          portname = args[0];
-          unitid = Integer.parseInt(args[1]);
-          ref = Integer.parseInt(args[2]);
-          count = Integer.parseInt(args[3]);
-          if (args.length == 5) {
-            repeat = Integer.parseInt(args[4]);
-          }
-        } catch (Exception ex) {
-          ex.printStackTrace();
-          printUsage();
-          System.exit(1);
-        }
-      }
+			// 2. Set slave identifier for master response parsing
+			ModbusCoupler.getReference().setUnitID(unitid);
 
-      //2. Set slave identifier for master response parsing
-      ModbusCoupler.getReference().setUnitID(unitid);
+			System.out.println("com.ghgande.j2mod.modbus.debug set to: "
+					+ System.getProperty("com.ghgande.j2mod.modbus.debug"));
 
-      System.out.println("com.ghgande.j2mod.modbus.debug set to: " +
-                         System.getProperty("com.ghgande.j2mod.modbus.debug"));
+			// 3. Setup serial parameters
+			SerialParameters params = new SerialParameters();
+			params.setPortName(portname);
+			params.setBaudRate(115200);
+			params.setDatabits(7);
+			params.setParity("None");
+			params.setStopbits(2);
+			// params.setEncoding("rtu");
+			// params.setEcho(true);
+			if (Modbus.debug)
+				System.out.println("Encoding [" + params.getEncoding() + "]");
 
-      //3. Setup serial parameters
-      SerialParameters params = new SerialParameters();
-      params.setPortName(portname);
-      params.setBaudRate(115200);
-      params.setDatabits(7);
-      params.setParity("None");
-      params.setStopbits(2);
-//       params.setEncoding("rtu");
-//       params.setEcho(true);
-      if (Modbus.debug) System.out.println("Encoding [" + params.getEncoding() + "]");
+			// 4. Open the connection
+			con = new SerialConnection(params);
+			con.open();
 
-      //4. Open the connection
-      con = new SerialConnection(params);
-      con.open();
+			// 5. Prepare a request
+			req = new ReadInputDiscretesRequest(ref, count);
+			req.setUnitID(unitid);
+			req.setHeadless();
+			if (Modbus.debug)
+				System.out.println("Request: " + req.getHexMessage());
 
+			// 6. Prepare the transaction
+			trans = new ModbusSerialTransaction(con);
+			trans.setRequest(req);
 
-      //5. Prepare a request
-      req = new ReadInputDiscretesRequest(ref, count);
-      req.setUnitID(unitid);
-      req.setHeadless();
-      if (Modbus.debug) System.out.println("Request: " + req.getHexMessage());
+			// 7. Execute the transaction repeat times
+			int k = 0;
+			do {
+				trans.execute();
 
-      //6. Prepare the transaction
-      trans = new ModbusSerialTransaction(con);
-      trans.setRequest(req);
+				res = (ReadInputDiscretesResponse) trans.getResponse();
+				if (Modbus.debug)
+					System.out.println("Response: " + res.getHexMessage());
+				BitVector inputs = res.getDiscretes();
+				for (int i = 0; i < count; i++) {
+					System.out.println("Bit " + i + " = " + inputs.getBit(i));
+				}
 
-      //7. Execute the transaction repeat times
-      int k = 0;
-      do {
-        trans.execute();
+				k++;
+			} while (k < repeat);
 
-        res = (ReadInputDiscretesResponse) trans.getResponse();
-        if (Modbus.debug) System.out.println("Response: " + res.getHexMessage());
-        BitVector inputs = res.getDiscretes();
-        byte ret[] = new byte[inputs.size()];
-        for (int i = 0; i < count; i++) {
-          System.out.println("Bit " + i + " = " + inputs.getBit(i));
-        }
+			// 8. Close the connection
+			con.close();
 
-        k++;
-      } while (k < repeat);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			// Close the connection
+			con.close();
+		}
+	}
 
-      //8. Close the connection
-      con.close();
-
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      // Close the connection
-      con.close();
-    }
-  }//main
-
-  private static void printUsage() {
-    System.out.println(
-        "java com.ghgande.j2mod.modbus.cmd.SerialAITest <portname [String]>  <Unit Address [int8]> <register [int16]> <wordcount [int16]> {<repeat [int]>}"
-    );
-  }//printUsage
-
-}//class SerialAITest
+	private static void printUsage() {
+		System.out.println(
+				"java com.ghgande.j2mod.modbus.cmd.SerialAITest <portname [String]>  <Unit Address [int8]> <register [int16]> <wordcount [int16]> {<repeat [int]>}");
+	}
+}
