ee8d095d58b115ef28ec47008860ee86b4240fd4
jfhaugh <jfhaugh@43b03309-50c7-4bd2-8aa6-20f049331b16>
2012-06-29 12:23:14 +0000

    Add read / write multiple registers messages.
    git-svn-id: svn://svn.code.sf.net/p/j2mod/code/trunk@24 43b03309-50c7-4bd2-8aa6-20f049331b16

--- src/com/ghgande/j2mod/modbus/msg/WriteMultipleRegistersRequest.java
+++ src/com/ghgande/j2mod/modbus/msg/WriteMultipleRegistersRequest.java
@@ -31,6 +31,38 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  ***/
+/**
+ *  * Java Modbus Library 2 (j2mod)
+ * Copyright (c) 2010-2012, greenHouse Gas and Electric
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the author nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS
+ * IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.ghgande.j2mod.modbus.msg;
 
 import java.io.DataInput;
@@ -43,13 +75,16 @@ import com.ghgande.j2mod.modbus.io.NonWordDataHandler;
 import com.ghgande.j2mod.modbus.procimg.*;
 
 /**
- * Class implementing a <tt>ReadMultipleRegistersRequest</tt>. The
+ * Class implementing a <tt>WriteMultipleRegistersRequest</tt>. The
  * implementation directly correlates with the class 0 function <i>write
  * multiple registers (FC 16)</i>. It encapsulates the corresponding request
  * message.
  * 
  * @author Dieter Wimberger
  * @version 1.2rc1 (09/11/2004)
+ * 
+ * @author jfhaugh
+ * @version @version@ (@date@)
  */
 public final class WriteMultipleRegistersRequest extends ModbusRequest {
 
